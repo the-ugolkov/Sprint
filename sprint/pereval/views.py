86cac683beeda9ee20780fd545181c59ebf7d1fe
Mikhail <113590874+the-ugolkov@users.noreply.github.com>
2023-04-11 20:15:09 +0700

    создал вью и указал урл через роутер

--- sprint/pereval/views.py
+++ sprint/pereval/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from pereval.models import PerevalAdded
+from pereval.serializers import PerevalAddedSerializer
+
+
+class PerevalViewSet(ModelViewSet):
+    queryset = PerevalAdded.objects.all()
+    serializer_class = PerevalAddedSerializer
+
--- sprint/sprint/urls.py
+++ sprint/sprint/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from pereval.views import PerevalViewSet
+
+router = SimpleRouter()
+router.register('pereval', PerevalViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += router.urls
