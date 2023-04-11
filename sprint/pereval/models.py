069426e1fd61669aee7dd9dee8307c1f43cb4b95
Mikhail <113590874+the-ugolkov@users.noreply.github.com>
2023-04-11 17:12:26 +0700

    создал модели по тз

--- sprint/pereval/models.py
+++ sprint/pereval/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+from pereval.resourses import STATUS
+
+
+class Users(models.Model):
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    otc = models.CharField(max_length=255)
+
+
+class PerevalAdded(models.Model):
+    user = models.ForeignKey('Users', on_delete=models.CASCADE)
+    beauty_title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    other_titles = models.CharField(max_length=255)
+    connect = models.TextField()
+    add_time = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    winter_level = models.CharField(max_length=100)
+    summer_level = models.CharField(max_length=100)
+    autumn_level = models.CharField(max_length=100)
+    spring_level = models.CharField(max_length=100)
+    coord_id = models.ForeignKey('Coords', on_delete=models.CASCADE)
+    images = models.ManyToManyField('PerevalImages')
+    status = models.CharField(choices=STATUS, default='new', max_length=8)
+
+
+class Coords(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    height = models.FloatField()
+
+
+class Level(models.Model):
+    pereval_added = models.ForeignKey(PerevalAdded, on_delete=models.CASCADE)
+    season = models.CharField(max_length=255)
+    difficulty_level = models.CharField(max_length=255)
+
+
+class PerevalImages(models.Model):
+    img = models.ImageField(upload_to='uploads/')
+    image_name = models.CharField(max_length=255)
+
+
+class PerevalAreas(models.Model):
+    id_parent = models.IntegerField()
+    title = models.TextField()
---
+++ sprint/pereval/resourses.py
@@ -0,0 +1,10 @@
+NEW = 'new'
+PENDING = 'pending'
+ACCEPTED = 'accepted'
+REJECTED = 'rejected'
+STATUS = [
+    ("new", "новый"),
+    ("pending",  "модератор взял в работу"),
+    ("accepted", "модерация прошла успешно"),
+    ("rejected",  "модерация прошла, информация не принята"),
+    ]
