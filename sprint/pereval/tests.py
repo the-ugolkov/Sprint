7d7e354fb7118cdf99195a80e4b38848cfa5f025
Mikhail <113590874+the-ugolkov@users.noreply.github.com>
2023-04-12 00:03:25 +0700

    добавил rest_framework  в настройки, добавил тесты(пока не работают)

--- sprint/pereval/tests.py
+++ sprint/pereval/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from .models import PerevalAdded
+
+
+class PerevalAddedTests(APITestCase):
+    def setUp(self):
+        self.url = reverse('submit_data')
+        self.valid_payload = {
+            'user': 1,
+            'beauty_title': 'Test Beauty Title',
+            'title': 'Test Title',
+            'other_titles': 'Test Other Titles',
+            'connect': 'Test Connect',
+            'winter_level': 'Test Winter Level',
+            'summer_level': 'Test Summer Level',
+            'autumn_level': 'Test Autumn Level',
+            'spring_level': 'Test Spring Level',
+            'coord_id': 1,
+            'images': [1, 2],
+            'status': 'new'
+        }
+        self.invalid_payload = {
+            'user': '',
+            'beauty_title': '',
+            'title': '',
+            'other_titles': '',
+            'connect': '',
+            'winter_level': '',
+            'summer_level': '',
+            'autumn_level': '',
+            'spring_level': '',
+            'coord_id': '',
+            'images': [],
+            'status': ''
+        }
+    print(PerevalAdded.objects.get().beauty_title)
+    def test_create_pereval_added_with_valid_payload(self):
+        data = self.valid_payload
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(PerevalAdded.objects.count(), 1)
+        self.assertEqual(PerevalAdded.objects.get().beauty_title, 'Test Beauty Title')
+
+    def test_create_pereval_added_with_invalid_payload(self):
+        data = self.invalid_payload
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(PerevalAdded.objects.count(), 0)
--- sprint/sprint/settings.py
+++ sprint/sprint/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pereval',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
