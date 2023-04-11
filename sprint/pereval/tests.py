73317cfd19ec1df5720218dc194a8d2339ee2520
Mikhail <113590874+the-ugolkov@users.noreply.github.com>
2023-04-12 01:26:25 +0700

    переписал тесты, теперь все работает)

--- sprint/pereval/tests.py
+++ sprint/pereval/tests.py
@@ -2,14 +2,18 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from .models import PerevalAdded
+from .models import PerevalAdded, Users, Coords, PerevalImages
 
 
 class PerevalAddedTests(APITestCase):
     def setUp(self):
         self.url = reverse('submit_data')
+        self.user = Users.objects.create(first_name='TestUser', last_name='TestUser', password='testpassword',
+                                         email='123', otc='TestUser')  # Создаем тестового пользователя
+        self.coord = Coords.objects.create(latitude=1.0, longitude=2.0, height=2.0)  # Создаем тестовые координаты
+        self.img = PerevalImages.objects.create(img='testimage.jpg', image_name='img')  # Создаем тестовое изображение
         self.valid_payload = {
-            'user': 2,
+            'user': self.user.pk,
             'beauty_title': 'Test Beauty Title',
             'title': 'Test Title',
             'other_titles': 'Test Other Titles',
@@ -18,8 +22,8 @@ class PerevalAddedTests(APITestCase):
             'summer_level': 'Test Summer Level',
             'autumn_level': 'Test Autumn Level',
             'spring_level': 'Test Spring Level',
-            'coord_id': 2,
-            'images': [2],
+            'coord_id': self.coord.pk,
+            'images': [self.img.pk],
             'status': 'new'
         }
         self.invalid_payload = {
