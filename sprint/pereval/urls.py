837ca4efc77c6a4f1c4860feb6fa0c89ec687457
Mikhail <113590874+the-ugolkov@users.noreply.github.com>
2023-04-19 15:23:32 +0700

    реализовал url для GET  функции

--- sprint/pereval/urls.py
+++ sprint/pereval/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import submit_data
+from .views import submit_data, get_data
 
 urlpatterns = [
     path('submitData/', submit_data, name='submit_data'),
+    path('submitData/<int:pk>/', get_data, name='get_data'),
 ]
