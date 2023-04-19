12c46cdc5ff6ae2f12c61ed7d7fcc6304db09af9
Mikhail <113590874+the-ugolkov@users.noreply.github.com>
2023-04-19 16:04:28 +0700

    покрыл тстами GET  функцию

--- sprint/pereval/tests.py
+++ sprint/pereval/tests.py
@@ -7,7 +7,8 @@ from .models import PerevalAdded, Users, Coords, PerevalImages
 
 class PerevalAddedTests(APITestCase):
     def setUp(self):
-        self.url = reverse('submit_data')
+        self.url_post = reverse('submit_data')
+        self.url_get = reverse('get_data', kwargs={'pk': 2})
         self.user = Users.objects.create(first_name='TestUser', last_name='TestUser', password='testpassword',
                                          email='TestEmail', otc='TestUser')
         self.coord = Coords.objects.create(latitude=1.0, longitude=2.0, height=2.0)
@@ -43,13 +44,36 @@ class PerevalAddedTests(APITestCase):
 
     def test_create_pereval_added_with_valid_payload(self):
         data = self.valid_payload
-        response = self.client.post(self.url, data, format='json')
+        response = self.client.post(self.url_post, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(PerevalAdded.objects.count(), 1)
         self.assertEqual(PerevalAdded.objects.get().beauty_title, 'Test Beauty Title')
 
     def test_create_pereval_added_with_invalid_payload(self):
         data = self.invalid_payload
-        response = self.client.post(self.url, data, format='json')
+        response = self.client.post(self.url_post, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(PerevalAdded.objects.count(), 0)
+
+    def test_get_existing_pereval(self):
+        user = self.user
+        coord = self.coord
+        pereval = PerevalAdded.objects.create(user=user,
+                                              beauty_title='Test Beauty Title',
+                                              title='Test Title',
+                                              other_titles='Test Other Titles',
+                                              connect='Test Connect',
+                                              winter_level='Test Winter Level',
+                                              summer_level='Test Summer Level',
+                                              autumn_level='Test Autumn Level',
+                                              spring_level='Test Spring Level',
+                                              coord_id=coord,
+                                              status='new')
+        response = self.client.get(self.url_get)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(PerevalAdded.objects.get().beauty_title, 'Test Beauty Title')
+
+    def test_get_not_existing_pereval(self):
+        url = reverse('get_data', kwargs={'pk': 2})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
